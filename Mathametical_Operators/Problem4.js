28c6dc7b0face99e036a1a270b9ad02e66617382
rishiaj22 <shrivastavarishiraj198@gmail.com>
2023-10-25 13:49:13 +0530

    Day 2

---
+++ Mathametical_Operators/Problem4.js
@@ -0,0 +1,9 @@
+/* You are given six numbers, stored in a variable with the following names
+one, two, three, four, five, six
+Print the sum of six numbers
+*/
+function sumOfFive(one, two, three, four, five) {
+    let sum = one+two+three+four+five;
+    console.log(sum);
+}
+
